b3749e0c2e7536ce379e43462b8532ab6a0aff6b
Ilya McCune-Pedit <ilyamcnpdt@protonmail.com>
2024-07-01 19:06:35 +0200

    fix inputs to fresnel for conductors

--- src/Materials/Conductor.cpp
+++ src/Materials/Conductor.cpp
@@ -14,7 +14,7 @@ bool Conductor::BSDF(const glm::vec3 &inRay, HitData &hit, glm::vec3 &scatterRay
 
 void Conductor::Color(const glm::vec3 &inRay, HitData &hit) const
 {
-    float f = Math::Fresnel(glm::dot(inRay, hit.normal), indexOfRefraction);
+    float f = Math::Fresnel(glm::dot(inRay, -hit.normal), 1/indexOfRefraction);
     // hit.color = color;
     hit.color = (1-f) * color + glm::vec3(1) * f;
 }
