b16e1f82aca915b394df8866328599d1351a5c3e
Ilya McCune-Pedit <ilyamcnpdt@protonmail.com>
2024-12-25 23:10:01 -0500

    cmake...

--- bin/CompileShaders.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env sh
-
-glslc Barley/Shaders/shader.vert -o build/shaders/vert.spv
-glslc Barley/Shaders/shader.frag -o build/shaders/frag.spv
--- init.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env sh
-
-premake5 gmake2
-premake5 ecc
